51f877c20fc5252ad6f6e11a5d20df5a73eb930d
okidoki99 <okidoki99@gmail.com>
2021-11-21 19:39:22 +0200

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,1 +1,26 @@
+import * as Dapp from "@elrondnetwork/dapp";
 
+export const dAppName = "ENRA";
+export const decimals = 2;
+export const denomination = 18;
+export const gasPrice = 1000000000;
+export const version = 1;
+export const gasLimit = 50000;
+export const gasPerDataByte = 1500;
+
+export const walletConnectBridge = "https://bridge.walletconnect.org";
+export const walletConnectDeepLink =
+  "https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/";
+
+export const contractAddress =
+  "erd1qqqqqqqqqqqqqpgquvt728n40ssd8n2qns9jrlqpwq2jc4rj4cysfuj3ad";
+
+export const network: Dapp.NetworkType = {
+  id: "devnet",
+  name: "Devnet",
+  egldLabel: "EGLD",
+  walletAddress: "https://wallet.elrond.com",
+  apiAddress: "https://api.elrond.com",
+  gatewayAddress: "https://gateway.elrond.com",
+  explorerAddress: "http://explorer.elrond.com/",
+};
