6beea5b70bf7d149163cf59e6022d991f3e7d928
okidoki99 <okidoki99@gmail.com>
2021-12-04 15:36:46 +0200

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
-import {
-  Switch,
-  Route,
-  useRouteMatch
-} from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import TopInfo from "./TopInfo";
 
 const Dashboard = () => {
