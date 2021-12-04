ceab8b178ac0e4bdcb20a9e2a55fbf9007753584
okidoki99 <okidoki99@gmail.com>
2021-12-04 15:09:32 +0200

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
-/*import * as Dapp from "@elrondnetwork/dapp";
-import { faBan } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import PageState from "components/PageState";
-import { contractAddress } from "config";
-import { useContext, useDispatch } from "context";*/
+import { useParams } from "react-router-dom";
 import TopInfo from "./TopInfo";
 
 const Dashboard = () => {
