9afb5b2e751f54f1beadefa3826ca452e44156a1
okidoki99 <okidoki99@gmail.com>
2021-12-05 21:15:54 +0200

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,7 +8,7 @@ import {
 import TopInfo from "./TopInfo";
 
 const query = new URLSearchParams(this.props.location.search);
-const token = query.get('token');
+const token = query.get("token");
 console.log(token);
 const Dashboard = () => {
   const ref = React.useRef(null);
