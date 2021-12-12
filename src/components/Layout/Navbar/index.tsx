c165fa587ec8f6bf0a526ed8844cebb1b26ea2b3
Mihai Bodnariu <mihai.bodnariu@butonium.com>
2021-12-12 18:22:11 +0200

    Change logout button text

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           {loggedIn && (
             <NavItem>
               <a href="/" onClick={logOut}>
-                Close
+                Disconnect
               </a>
             </NavItem>
           )}
