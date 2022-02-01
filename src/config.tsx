2e7ffadf4d5fd5facce46e916df6633f94424a73
Mihai Bodnariu <mihai.bodnariu@butonium.com>
2022-02-01 09:00:57 +0200

    fix: replaced chain value

--- src/config.tsx
+++ src/config.tsx
@@ -10,14 +10,14 @@ export const gasPerDataByte = 1500;
 
 export const walletConnectBridge = "https://bridge.walletconnect.org";
 export const walletConnectDeepLink =
-  "https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/";
+  "https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet&link=https://maiar.com/";
 
 export const contractAddress =
   "erd1qqqqqqqqqqqqqpgquvt728n40ssd8n2qns9jrlqpwq2jc4rj4cysfuj3ad";
 
 export const network: Dapp.NetworkType = {
-  id: "devnet",
-  name: "Devnet",
+  id: "mainnet",
+  name: "Mainnet",
   egldLabel: "EGLD",
   walletAddress: "https://wallet.elrond.com",
   apiAddress: "https://api.elrond.com",
